ab91d23035a5ae011bc7cc574d11c99e893bb992
JornH <j.hertsig@topdesk.com>
2019-06-04 10:16:40 +0200

    New mod: WhistleStop Extended

---
+++ WhistleStop-extended/README.md
@@ -0,0 +1,7 @@
+WhistleStop Extended
+-------------------------------------
+
+Allows you to create new WhistleStop factory buildings, and to move existing ones
+
+Big building icon borrowed from Factorissimo2
+ 
---
+++ WhistleStop-extended/settings.lua
@@ -0,0 +1,22 @@
+data:extend({
+    {
+        type = "bool-setting",
+        name = "wsfx-minable",
+        default_value = true,
+        setting_type = "startup"
+    },
+    {
+        type = "bool-setting",
+        name = "wsfx-craftable",
+        default_value = true,
+        setting_type = "startup"
+    },
+    {
+        type = "int-setting",
+        name = "wsfx-module-slots",
+        default_value = 6,
+        minimum_value = 0,
+        maximum_value = 12,
+        setting_type = "startup"
+    },
+})
