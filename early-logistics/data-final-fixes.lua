01dc53e11b8ea91c271025d13a38e3e12d5fac2e
Jorn86 <factoriomods@hertsig.org>
2019-08-07 12:44:17 +0200

    Early logistics: Update tech dependency, and release 0.0.2

--- early-logistics/data-final-fixes.lua
+++ early-logistics/data-final-fixes.lua
@@ -1,17 +1,18 @@
 local function removeUtility(name)
     local tech = data.raw.technology[name]
-    local ingredients = tech.unit.ingredients
-    local remove = -1
-    for i,ingredient in pairs(ingredients) do
-        if ingredient[1] == 'utility-science-pack' then remove = i end
+
+    for i, item in pairs(tech.unit.ingredients) do
+        if item[1] == 'utility-science-pack' then
+            table.remove(tech.unit.ingredients, i)
+        end
     end
-    if remove > 0 then
-        local removed = table.remove(ingredients, remove)
-        if removed[1] ~= 'utility-science-pack' then error() end
+
+    for i, prereq in pairs(tech.prerequisites) do
+        if prereq == 'utility-science-pack' then
+            tech.prerequisites[i] = 'chemical-science-pack'
+        end
     end
 end
 
+removeUtility('logistic-robotics')
 removeUtility('logistic-system')
-if mods['space-exploration'] then
-    removeUtility('logistic-robotics')
-end
