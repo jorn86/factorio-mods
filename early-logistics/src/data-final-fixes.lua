10f6c570312db253a2735b68229469a6e0ff1e9e
Jorn86 <factoriomods@hertsig.org>
2019-09-10 15:59:24 +0200

    Early logistics: Add IndustrialRevolution compatibility, and release 0.0.3

--- early-logistics/src/data-final-fixes.lua
+++ early-logistics/src/data-final-fixes.lua
@@ -1,18 +1,30 @@
-local function removeUtility(name)
+local function removePacks(name)
     local tech = data.raw.technology[name]
+    if not tech then
+        print(name .. ' does not exist')
+        return
+    end
 
     for i, item in pairs(tech.unit.ingredients) do
         if item[1] == 'utility-science-pack' then
             table.remove(tech.unit.ingredients, i)
         end
+        if item[1] == 'production-science-pack' then
+            table.remove(tech.unit.ingredients, i)
+        end
     end
 
-    for i, prereq in pairs(tech.prerequisites) do
-        if prereq == 'utility-science-pack' then
-            tech.prerequisites[i] = 'chemical-science-pack'
+    if tech.prerequisites then
+        for i, prereq in pairs(tech.prerequisites) do
+            if prereq == 'utility-science-pack' then
+                tech.prerequisites[i] = 'chemical-science-pack'
+            end
+            if prereq == 'production-science-pack' then
+                tech.prerequisites[i] = 'chemical-science-pack'
+            end
         end
     end
 end
 
-removeUtility('logistic-robotics')
-removeUtility('logistic-system')
+removePacks('logistic-robotics')
+removePacks('logistic-system')
